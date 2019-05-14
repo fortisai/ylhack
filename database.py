74354bdf0ff6b75eb92613e518dd9bf6f65dbec6
lollihunter <killrealm@mail.ru>
2019-05-14 10:13:57 +0300

    12345

--- database.py
+++ database.py
@@ -22,6 +22,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(50), unique=False, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=True)
     admin = db.Column(db.Boolean, unique=False, nullable=False)
 
 
@@ -31,6 +32,11 @@ class Problem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     statement = db.Column(db.String(5000), unique=False, nullable=False)
     time_end = db.Column(db.DateTime, unique=False, nullable=False)
+    author_id = db.Column(db.Integer, unique=False, nullable=False)
+    solver_id = db.Column(db.Integer, unique=False, nullable=True)
+    priority = db.Column(db.Integer, unique=False, nullable=True)
+    completion_stage = db.Column(db.Integer, unique=False, nullable=True)
+    category = db.Column(db.String(100), unique=False, nullable=True)
 
 
 db.create_all()
